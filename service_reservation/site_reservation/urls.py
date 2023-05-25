e0c716ce278217d1cbd3c6e18c29319bcb7eed50
daour01 <daoursambandaw@gmail.com>
2023-05-25 16:13:51 +0000

    modif

---
+++ service_reservation/site_reservation/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('', views.index, name='index'), 
+        path("about", views.about, name='about'),
+        path("contact", views.contact, name='contact'),
+        path("voiture", views.voiture, name='voiture'),
+        path("chambre", views.chambre, name='chambre'),
+        path("restaurant", views.restaurant, name='restaurant'),
+        path("detailvoiture", views.detailvoiture, name='detailvoiture'),
+        path("connection", views.connection, name='connection')
+    ] 
---
+++ service_reservation/site_reservation/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+
+def index(request):
+    context = {"message": "hello word"}
+    template = loader.get_template("site_reservation/index.html")
+    return HttpResponse(template.render(context, request))
+
+def about(request):
+    context={}
+    template = loader.get_template("site_reservation/about.html")
+    return HttpResponse(template.render(context,request))
+
+def contact(request):
+    context={}
+    template = loader.get_template("site_reservation/contact.html")
+    return HttpResponse(template.render(context,request))
+
+def voiture(request):
+    context={}
+    template = loader.get_template("site_reservation/voiture.html")
+    return HttpResponse(template.render(context,request))
+
+def chambre(request):
+    context={}
+    template = loader.get_template("site_reservation/chambre.html")
+    return HttpResponse(template.render(context,request))
+
+def restaurant(request):
+    context={}
+    template = loader.get_template("site_reservation/restaurant.html")
+    return HttpResponse(template.render(context,request))
+
+def detailvoiture(request):
+    context={}
+    template = loader.get_template("site_reservation/detailvoiture.html")
+    return HttpResponse(template.render(context,request))
+
+def connection(request):
+    context={}
+    template = loader.get_template("site_reservation/connection.html")
+    return HttpResponse(template.render(context,request))
