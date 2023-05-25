08ae76a24a9fd5064dcaccd111ec568ed9c3c66c
khalil6c <cissekhalil15@gmail.com>
2023-05-25 13:28:35 +0000

    page_accueil service_ chambre page_connexion page_paiement

---
+++ service_reservation/site_reservation/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+
+# Create your models here.
+
+
+from django.db import models
+
+class Voiture(models.Model):
+    marque = models.CharField(max_length=100)
+    modele = models.CharField(max_length=100)
+    nombre_places = models.IntegerField()
+    boite_vitesse = models.CharField(max_length=100)
+    carburant = models.CharField(max_length=100)
+    climatisation = models.BooleanField(default=False)
+    couleur = models.CharField(max_length=100)
+    annee_production = models.IntegerField()
+    cout_journalier_location = models.IntegerField()
+
+
+
+
+class Hotel(models.Model):
+    nom = models.CharField(max_length=100)
+    adresse = models.CharField(max_length=100)
+
+
+
+
+import requests
+import json
+import mysql.connector
+
+# Informations de connexion à la base de données MySQL
+host = '127.0.0.1'
+database = 'gestion_reservation'
+user = 'khalil'
+password = 'Kh@lil6c'
+
+# Connexion à la base de données MySQL
+cnx = mysql.connector.connect(host=host, database=database, user=user, password=password)
+cursor = cnx.cursor()
+
+# Définition de l'URL de l'API et du token
+url = 'https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=dkr&radius=300&radiusUnit=KM&hotelSource=ALL'
+token = 's6wA5ozyK7zqLRrUVb6qFzA5xl9F'
+
+# Ajout du token à l'en-tête de la requête
+headers = {
+    'Authorization': f'Bearer {token}'
+}
+
+# Envoi de la requête GET à l'API
+response = requests.get(url, headers=headers)
+
+# Vérification du statut de la réponse
+if response.status_code == 200:
+    # Conversion de la réponse en JSON
+    data = response.json()
+
+    # Récupération des données de l'API
+    hotels = data.get('data', [])
+
+    # Insertion des données dans la table de la base de données
+    for hotel in hotels:
+        hotel_name = hotel['name']
+        hotel_address = hotel['iataCode']
+
+        query = "INSERT INTO site_reservation_hotel (nom, adresse) VALUES (%s, %s)"
+        values = (hotel_name, json.dumps(hotel_address))
+
+        cursor.execute(query, values)
+
+    cnx.commit()
+    print("Données insérées avec succès dans la base de données.")
+else:
+    print(f"Erreur lors de la requête : {response.status_code}")
+
+# Fermeture de la connexion à la base de données
+cursor.close()
+cnx.close()
---
+++ service_reservation/site_reservation/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+
+def index(request):
+    context = {"message": "hello word"}
+    template = loader.get_template("site_reservation/index.html")
+    return HttpResponse(template.render(context, request))
+
+def about(request):
+    context={}
+    template = loader.get_template("site_reservation/about.html")
+    return HttpResponse(template.render(context,request))
+
+def contact(request):
+    context={}
+    template = loader.get_template("site_reservation/contact.html")
+    return HttpResponse(template.render(context,request))
+
+def voiture(request):
+    context={}
+    template = loader.get_template("site_reservation/voiture.html")
+    return HttpResponse(template.render(context,request))
+
+def chambre(request):
+    context={}
+    template = loader.get_template("site_reservation/chambre.html")
+    return HttpResponse(template.render(context,request))
+
+def restaurant(request):
+    context={}
+    template = loader.get_template("site_reservation/restaurant.html")
+    return HttpResponse(template.render(context,request))
+
+def detailvoiture(request):
+    context={}
+    template = loader.get_template("site_reservation/detailvoiture.html")
+    return HttpResponse(template.render(context,request))
+
+def connection(request):
+    context={}
+    template = loader.get_template("site_reservation/connection.html")
+    return HttpResponse(template.render(context,request))
+
+
+
+# import requests
+# from datetime import datetime
+# def search_results(request):
+#     date_debut_str = request.GET.get('date_debut')
+#     date_fin_str = request.GET.get('date_fin')
+
+#     if date_debut_str and date_fin_str:
+#         date_debut = datetime.strptime(date_debut_str, '%m/%d/%Y').date()
+#         date_fin = datetime.strptime(date_fin_str, '%m/%d/%Y').date()
+
+#         # Effectuer une requête HTTP vers l'API 'api-car' avec les dates de début et de fin
+#         response = requests.get('https://localhost:8080/api/cars', params={'date_debut': date_debut_str, 'date_fin': date_fin_str})
+
+#         if response.status_code == 200:
+#             voitures = response.json()
+#             context = {'voitures': voitures, 'date_debut': date_debut, 'date_fin': date_fin}
+#             return render(request, 'search_results.html', context)
+#         else:
+#             return render(request, 'error.html')
+
+#     return render(request, 'search_results.html')
+
+
+
+import json
+
+def search_results(request):
+    # Charger le fichier JSON
+    with open('donnees_voitures.json') as json_file:
+        data = json.load(json_file)
+
+    date_debut_str = request.GET.get('date_debut')
+    date_fin_str = request.GET.get('date_fin')
+
+    # Accéder aux données pertinentes dans le fichier JSON
+    voitures = data['voitures']  # Remplacez 'voitures' par la clé appropriée dans votre fichier JSON
+
+    # Effectuer d'autres opérations avec les données
+
+    context = {'voitures': voitures}
+    return render(request, 'search_results.html', context)
