0e81ac5b0992e76690bd7bbd166b4d3b21daf3ff
daour01 <daoursambandaw@gmail.com>
2023-05-31 13:13:25 +0000

    modif

--- service_reservation/site_reservation/models.py
+++ service_reservation/site_reservation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Resto(models.Model):
+    emplacement = models.CharField(max_length=100)
+    type_table = models.CharField(max_length=100)
+    couleur_nappe = models.CharField(max_length=100)
+    cout_reservation = models.DecimalField(max_digits=10, decimal_places=2)
+    imgs = models.TextField()
